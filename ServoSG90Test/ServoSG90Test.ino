c733c5471cea4065f1984fe58e7dcc152d042607
Yusuke Nakamura <yusuke1994525@gmail.com>
2014-11-12 14:29:59 +0900

    Servo Tick-Tack
    0°→90°を交互に

--- ServoSG90Test/ServoSG90Test.ino
+++ ServoSG90Test/ServoSG90Test.ino
@@ -51,11 +51,11 @@ void setup(){
 }
 
 void loop(){
-  for( int i = 0; i <= 120; i+=5){
-    for( int j = 0; j < 4; j++ ){
-      setServoPulse( j , degree2Pulse( i ) );
+  for( int i = 0; i <= 120; i+=30){
+    for( int j = 0; j < 12; j++ ){
+      setServoPulse( j , degree2Pulse( i < 90?0:90 ) );
     }
-    delay(25);
+    //delay(1);
     if( i > 119.0 ) i = 0;
     else;
   }
